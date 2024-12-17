40f852ad7149bdddb6b6e1b55cb41f4ece698cf7
kieran <kieran@harkin.me>
2024-12-17 10:09:29 +0000

    feat: auto-ingest / config

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 use anyhow::Result;
 use async_compression::tokio::write::ZstdEncoder;
 use chrono::{DateTime, Utc};
-use log::{error, info};
-use nostr_relay_builder::builder::{PolicyResult, QueryPolicy, RateLimit};
+use clap::Parser;
+use config::{Config, ConfigBuilder};
+use log::{debug, error, info};
+use nostr_relay_builder::builder::{PolicyResult, QueryPolicy, RateLimit, WritePolicy};
 use nostr_relay_builder::prelude::{
     async_trait, Backend, Coordinate, DatabaseError, DatabaseEventStatus, Event, EventId, Events,
-    JsonUtil, NostrDatabase, RejectedReason, Timestamp,
+    JsonUtil, Kind, NostrDatabase, RejectedReason, Timestamp,
 };
 use nostr_relay_builder::prelude::{
     Filter as RelayFilter, NostrEventsDatabase, RelayUrl, SaveEventStatus,
 };
 use nostr_relay_builder::{LocalRelay, RelayBuilder};
+use nostr_sdk::prelude::StreamExt;
+use nostr_sdk::{Client, FilterOptions};
+use serde::Deserialize;
 use std::collections::HashSet;
 use std::fmt::{Debug, Formatter};
 use std::fs::create_dir_all;
@@ -18,6 +23,7 @@ use std::io::{Error, ErrorKind};
 use std::net::{IpAddr, SocketAddr};
 use std::path::PathBuf;
 use std::sync::Arc;
+use std::time::Duration;
 use tokio::fs::{File, OpenOptions};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::sync::Mutex;
@@ -31,6 +37,20 @@ struct NoQuery;
 impl QueryPolicy for NoQuery {
     async fn admit_query(&self, _query: &[RelayFilter], _addr: &SocketAddr) -> PolicyResult {
         PolicyResult::Reject("queries not allowed".to_string())
+    }
+}
+
+#[derive(Debug)]
+struct KindPolicy(HashSet<Kind>);
+
+#[async_trait]
+impl WritePolicy for KindPolicy {
+    async fn admit_event(&self, event: &Event, _addr: &SocketAddr) -> PolicyResult {
+        if self.0.contains(&event.kind) {
+            PolicyResult::Accept
+        } else {
+            PolicyResult::Reject("Kind not accepted".to_string())
+        }
     }
 }
 
@@ -116,6 +136,7 @@ impl FlatFileWriter {
     }
 }
 
+#[derive(Clone)]
 struct FlatFileDatabase {
     database: sled::Db,
     dir: PathBuf,
@@ -160,6 +181,7 @@ impl NostrEventsDatabase for FlatFileDatabase {
                 self.write_event(event).await.map_err(|e| {
                     DatabaseError::Backend(Box::new(Error::new(ErrorKind::Other, e)))
                 })?;
+                debug!("Saved event: {}", event.id);
                 Ok(SaveEventStatus::Success)
             }
             _ => Ok(SaveEventStatus::Rejected(RejectedReason::Duplicate)),
@@ -229,20 +251,84 @@ impl NostrDatabase for FlatFileDatabase {
     }
 }
 
+#[derive(Parser)]
+#[command(version, about)]
+struct Args {
+    /// Define path for config file
+    pub config: Option<PathBuf>,
+}
+
+#[derive(Deserialize)]
+struct Settings {
+    /// Listen address for relay ip:port
+    pub listen_relay: Option<String>,
+
+    /// Nostr relays to ingest events from
+    pub relays: Option<Vec<String>>,
+
+    /// Nostr kinds to accept
+    pub kinds: Option<Vec<u32>>,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     pretty_env_logger::init();
 
+    let args = Args::parse();
+
+    let config: Settings = Config::builder()
+        .add_source(config::File::from(
+            args.config.unwrap_or(PathBuf::from("config.yaml")),
+        ))
+        .build()?
+        .try_deserialize()?;
+
     let out_dir = PathBuf::from("./data");
-    let b = RelayBuilder::default()
-        .database(FlatFileDatabase::new(out_dir.clone())?)
-        .addr(IpAddr::from([0, 0, 0, 0]))
-        .port(8001)
+    let addr: SocketAddr = config
+        .listen_relay
+        .map(|a| a.parse())
+        .unwrap_or(Ok(SocketAddr::from(([0, 0, 0, 0], 8001))))?;
+
+    let db = FlatFileDatabase::new(out_dir.clone())?;
+    let client = Client::builder().database(db.clone()).build();
+    if let Some(r) = config.relays {
+        for r in r {
+            client.add_relay(r).await?;
+        }
+        client.connect().await;
+        let never = Duration::from_secs(u64::MAX);
+        let mut stream = client
+            .pool()
+            .stream_events(
+                vec![nostr_sdk::Filter::new().limit(100)],
+                never,
+                FilterOptions::WaitDurationAfterEOSE(never),
+            )
+            .await?;
+        let db = db.clone();
+        tokio::spawn(async move {
+            while let Some(event) = stream.next().await {
+                if let Err(e) = db.save_event(&event).await {
+                    error!("Failed to save event: {}", e);
+                }
+            }
+        });
+    }
+    let mut b = RelayBuilder::default()
+        .database(db.clone())
+        .addr(addr.ip())
+        .port(addr.port())
         .query_policy(NoQuery)
         .rate_limit(RateLimit {
             max_reqs: 20,
             notes_per_minute: 100_000,
         });
+
+    if let Some(k) = config.kinds {
+        b = b.write_policy(KindPolicy(
+            k.iter().map(|k| Kind::Custom(*k as u16)).collect(),
+        ));
+    }
 
     let relay = LocalRelay::run(b).await?;
 
